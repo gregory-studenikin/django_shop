ccdbf1ed6c0c693271c2ec7c8e0069d8e6c306d9
gregory-studenikin <grisha-faulty@yandex.ru>
2022-06-25 06:22:58 +0300

    Create login form

--- shop_project/authentication/views.py
+++ shop_project/authentication/views.py
@@ -1,10 +1,22 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
+
+from .forms import LoginForm
 
 
 def login_user(request):
-    return render(request, template_name='auth/login.html')
+    context = {'login_form': LoginForm()}
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
 
+    return render(request, 'auth/login.html', context=context)
 
 def register(request):
     return render(request, template_name='auth/register.html')
